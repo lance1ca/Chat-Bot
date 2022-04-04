fbc86d8f5bba45f93705160aeb498ae62236379e
Lance Rogan <77311869+lance1ca@users.noreply.github.com>
2022-04-04 15:43:08 -0700

    Adding secured Token environment variables for API Access

---
+++ ChatBot Individual Project/src/main/java/group10/TranslateText.java
@@ -0,0 +1,53 @@
+package group10;
+// Imports the Google Cloud Translation library.
+import com.google.cloud.translate.v3.LocationName;
+import com.google.cloud.translate.v3.TranslateTextRequest;
+import com.google.cloud.translate.v3.TranslateTextResponse;
+import com.google.cloud.translate.v3.Translation;
+import com.google.cloud.translate.v3.TranslationServiceClient;
+import java.io.IOException;
+
+
+public class TranslateText {
+
+  // Set and pass variables to overloaded translateText() method for translation.
+  public static void translateText() throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    String projectId = "YOUR-PROJECT-ID";
+    // Supported Languages: https://cloud.google.com/translate/docs/languages
+    String targetLanguage = "english";
+    String text = "Hola";
+    translateText(projectId, targetLanguage, text);
+  }
+
+  // Translate text to target language.
+  public static void translateText(String projectId, String targetLanguage, String text)
+      throws IOException {
+
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (TranslationServiceClient client = TranslationServiceClient.create()) {
+      // Supported Locations: `global`, [glossary location], or [model location]
+      // Glossaries must be hosted in `us-central1`
+      // Custom Models must use the same location as your model. (us-central1)
+      LocationName parent = LocationName.of(projectId, "global");
+
+      // Supported Mime Types: https://cloud.google.com/translate/docs/supported-formats
+      TranslateTextRequest request =
+          TranslateTextRequest.newBuilder()
+              .setParent(parent.toString())
+              .setMimeType("text/plain")
+              .setTargetLanguageCode(targetLanguage)
+              .addContents(text)
+              .build();
+
+      TranslateTextResponse response = client.translateText(request);
+
+      // Display the translation for each input text provided
+      for (Translation translation : response.getTranslationsList()) {
+        System.out.printf("Translated text: %s\n", translation.getTranslatedText());
+      }
+    }
+  }
+}
---
+++ ChatBot Individual Project/src/main/java/group10/environmentTests.java
@@ -0,0 +1,7 @@
+package group10;
+
+public class environmentTests {
+    public static void main(String[]args){
+        System.out.println(System.getenv("TESTENVIRONMENTVARIABLE2022"));
+    }
+}
